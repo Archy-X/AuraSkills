91e7aca1daf5854c07237b1c58224ae905330ded
Archy <63976867+Archy-X@users.noreply.github.com>
2020-07-23 20:37:18 -0700

    Update ItemUtils.java

--- src/main/java/io/github/archy_x/aureliumskills/util/ItemUtils.java
+++ src/main/java/io/github/archy_x/aureliumskills/util/ItemUtils.java
@@ -4,7 +4,7 @@ import org.bukkit.Material;
 
 public class ItemUtils {
 
-  public static boolean isArmor(Material material) {
+ 	public static boolean isArmor(Material material) {
 		String materialName = material.name().toLowerCase();
 		if (materialName.contains("helmet") || materialName.contains("chestplate") || materialName.contains("leggings") || materialName.contains("boots")) {
 			return true;
@@ -20,11 +20,11 @@ public class ItemUtils {
 		return false;
 	}
   
-  public static boolean isTool(Material material) {
-    String materialName = material.name().toLowerCase();
-    if (materialName.contains("pickaxe") || materialName.contains("axe") || materialName.contains("hoe") || materialName.contains("shovel") || materialName.contains("spade")) {
-      return true;
-    }
-    return false;
-  }
+  	public static boolean isTool(Material material) {
+    		String materialName = material.name().toLowerCase();
+    	if (materialName.contains("pickaxe") || materialName.contains("axe") || materialName.contains("hoe") || materialName.contains("shovel") || materialName.contains("spade")) {
+      		return true;
+    	}
+    	return false;
+  	}
 } 
