14bf078af52315d6dbcf2c3d558af0b3df565072
Archy-X <andrewxiearchway@gmail.com>
2020-08-13 16:30:06 -0700

    Added regen disabled world checks

--- src/main/java/com/archyx/aureliumskills/stats/Regeneration.java
+++ src/main/java/com/archyx/aureliumskills/stats/Regeneration.java
@@ -1,10 +1,12 @@
 package com.archyx.aureliumskills.stats;
 
+import com.archyx.aureliumskills.AureliumSkills;
 import com.archyx.aureliumskills.Options;
 import com.archyx.aureliumskills.Setting;
 import com.archyx.aureliumskills.skills.SkillLoader;
 import org.bukkit.Bukkit;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -15,7 +17,7 @@ import org.bukkit.plugin.Plugin;
 
 public class Regeneration implements Listener {
 
-	private Plugin plugin;
+	private final Plugin plugin;
 	
 	public Regeneration(Plugin plugin) {
 		this.plugin = plugin;
@@ -25,8 +27,12 @@ public class Regeneration implements Listener {
 	public void onRegen(EntityRegainHealthEvent event) {
 		if (event.getEntity() instanceof Player) {
 			if (event.getRegainReason().equals(RegainReason.SATIATED)) {
+				Player player = (Player) event.getEntity();
+				//Check for disabled world
+				if (AureliumSkills.worldManager.isInDisabledWorld(player.getLocation())) {
+					return;
+				}
 				if (!Options.getBooleanOption(Setting.CUSTOM_REGEN_MECHANICS)) {
-					Player player = (Player) event.getEntity();
 					if (SkillLoader.playerStats.containsKey(player.getUniqueId())) {
 						PlayerStat stat = SkillLoader.playerStats.get(player.getUniqueId());
 						if (player.getSaturation() > 0) {
@@ -48,32 +54,24 @@ public class Regeneration implements Listener {
 	}
 	
 	public void startRegen() {
-		Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
-			@Override
-			public void run() {
-				if (Options.getBooleanOption(Setting.CUSTOM_REGEN_MECHANICS)) {
-					for (Player player : Bukkit.getOnlinePlayers()) {
-						if (player.isDead() == false) {
-							if (player.getHealth() < player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue()) {
+		Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, () -> {
+			if (Options.getBooleanOption(Setting.CUSTOM_REGEN_MECHANICS)) {
+				for (Player player : Bukkit.getOnlinePlayers()) {
+					//Check for disabled world
+					if (AureliumSkills.worldManager.isInDisabledWorld(player.getLocation())) {
+						return;
+					}
+					if (!player.isDead()) {
+						AttributeInstance attribute = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+						if (attribute != null) {
+							if (player.getHealth() < attribute.getValue()) {
 								if (player.getFoodLevel() >= 14 && player.getFoodLevel() < 20) {
-									
-									if ((player.getHealth() + Options.getDoubleOption(Setting.BASE_REGEN) + (double) SkillLoader.playerStats.get(player.getUniqueId()).getStatLevel(Stat.REGENERATION) * Options.getDoubleOption(Setting.HUNGER_ALMOST_FULL_MODIFIER)) <= player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue()) {
-										player.setHealth(player.getHealth() + Options.getDoubleOption(Setting.BASE_REGEN) + (double) SkillLoader.playerStats.get(player.getUniqueId()).getStatLevel(Stat.REGENERATION) * Options.getDoubleOption(Setting.HUNGER_ALMOST_FULL_MODIFIER));
-									}
-									else {
-										player.setHealth(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
-									}
+									player.setHealth(Math.min((player.getHealth() + Options.getDoubleOption(Setting.BASE_REGEN) + (double) SkillLoader.playerStats.get(player.getUniqueId()).getStatLevel(Stat.REGENERATION) * Options.getDoubleOption(Setting.HUNGER_ALMOST_FULL_MODIFIER)), attribute.getValue()));
 									if (player.getFoodLevel() - 1 >= 0) {
 										player.setFoodLevel(player.getFoodLevel() - 1);
 									}
-								}
-								else if (player.getFoodLevel() == 20 && player.getSaturation() == 0) {
-									if ((player.getHealth() + Options.getDoubleOption(Setting.BASE_REGEN) + (double) SkillLoader.playerStats.get(player.getUniqueId()).getStatLevel(Stat.REGENERATION) * Options.getDoubleOption(Setting.HUNGER_FULL_MODIFIER)) <= player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue()) {
-										player.setHealth(player.getHealth() + Options.getDoubleOption(Setting.BASE_REGEN) + (double) SkillLoader.playerStats.get(player.getUniqueId()).getStatLevel(Stat.REGENERATION) * Options.getDoubleOption(Setting.HUNGER_FULL_MODIFIER));
-									}
-									else {
-										player.setHealth(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
-									}
+								} else if (player.getFoodLevel() == 20 && player.getSaturation() == 0) {
+									player.setHealth(Math.min((player.getHealth() + Options.getDoubleOption(Setting.BASE_REGEN) + (double) SkillLoader.playerStats.get(player.getUniqueId()).getStatLevel(Stat.REGENERATION) * Options.getDoubleOption(Setting.HUNGER_FULL_MODIFIER)), attribute.getValue()));
 									if (player.getFoodLevel() - 1 >= 0) {
 										player.setFoodLevel(player.getFoodLevel() - 1);
 									}
@@ -87,19 +85,14 @@ public class Regeneration implements Listener {
 	}
 	
 	public void startSaturationRegen() {
-		Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
-			@Override
-			public void run() {
-				if (Options.getBooleanOption(Setting.CUSTOM_REGEN_MECHANICS)) {
-					for (Player player : Bukkit.getOnlinePlayers()) {
-						if (player.isDead() == false) {
-							if (player.getSaturation() > 0 && player.getFoodLevel() >= 20) {
-								if ((player.getHealth() +  Options.getDoubleOption(Setting.BASE_REGEN) + (double) SkillLoader.playerStats.get(player.getUniqueId()).getStatLevel(Stat.REGENERATION) * Options.getDoubleOption(Setting.SATURATED_MODIFIER)) <= player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue()) {
-									player.setHealth(player.getHealth() + Options.getDoubleOption(Setting.BASE_REGEN) + (double) SkillLoader.playerStats.get(player.getUniqueId()).getStatLevel(Stat.REGENERATION) * Options.getDoubleOption(Setting.SATURATED_MODIFIER));
-								}
-								else {
-									player.setHealth(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
-								}
+		Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, () -> {
+			if (Options.getBooleanOption(Setting.CUSTOM_REGEN_MECHANICS)) {
+				for (Player player : Bukkit.getOnlinePlayers()) {
+					if (!player.isDead()) {
+						if (player.getSaturation() > 0 && player.getFoodLevel() >= 20) {
+							AttributeInstance attribute = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+							if (attribute != null) {
+								player.setHealth(Math.min((player.getHealth() + Options.getDoubleOption(Setting.BASE_REGEN) + (double) SkillLoader.playerStats.get(player.getUniqueId()).getStatLevel(Stat.REGENERATION) * Options.getDoubleOption(Setting.SATURATED_MODIFIER)), attribute.getValue()));
 							}
 						}
 					}
