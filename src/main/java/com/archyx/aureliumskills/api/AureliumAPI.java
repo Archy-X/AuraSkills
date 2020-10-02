14ac4401668812dcaf1a4eb94ddb0f087b1228c1
Archy-X <andrewxiearchway@gmail.com>
2020-10-02 11:27:18 -0700

    Added Stat methods to the API

--- src/main/java/com/archyx/aureliumskills/api/AureliumAPI.java
+++ src/main/java/com/archyx/aureliumskills/api/AureliumAPI.java
@@ -1,10 +1,13 @@
 package com.archyx.aureliumskills.api;
 
 import com.archyx.aureliumskills.AureliumSkills;
+import com.archyx.aureliumskills.modifier.StatModifier;
 import com.archyx.aureliumskills.skills.PlayerSkill;
 import com.archyx.aureliumskills.skills.Skill;
 import com.archyx.aureliumskills.skills.SkillLoader;
 import com.archyx.aureliumskills.skills.levelers.Leveler;
+import com.archyx.aureliumskills.stats.PlayerStat;
+import com.archyx.aureliumskills.stats.Stat;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
@@ -80,6 +83,12 @@ public class AureliumAPI {
         }
     }
 
+    /**
+     * Gets the skill xp of a player
+     * @param player The player to get from
+     * @param skill The skill to get
+     * @return The current skill xp
+     */
     public static double getXp(Player player, Skill skill) {
         PlayerSkill playerSkill = SkillLoader.playerSkills.get(player.getUniqueId());
         if (playerSkill != null) {
@@ -89,5 +98,69 @@ public class AureliumAPI {
             return 1;
         }
     }
+
+    /**
+     * Gets the stat level of a player
+     * @param player The player to get from
+     * @param stat The stat to get
+     * @return The stat level
+     */
+    public static int getStatLevel(Player player, Stat stat) {
+        PlayerStat playerStat = SkillLoader.playerStats.get(player.getUniqueId());
+        if (playerStat != null) {
+            return playerStat.getStatLevel(stat);
+        }
+        else {
+            return 0;
+        }
+    }
+
+    /**
+     * Gets the base stat level of a player, without modifiers
+     * @param player The player to get from
+     * @param stat The stat to get
+     * @return The stat level without modifiers
+     */
+    public static int getBaseStatLevel(Player player, Stat stat) {
+        PlayerStat playerStat = SkillLoader.playerStats.get(player.getUniqueId());
+        if (playerStat != null) {
+            return playerStat.getBaseStatLevel(stat);
+        }
+        else {
+            return 0;
+        }
+    }
+
+    /**
+     * Adds a stat modifier to a player
+     * @param player The player to add to
+     * @param name The name of the stat modifier
+     * @param stat The stat to add to
+     * @param value The value of the modifier
+     * @return true if a modifier was added, false if the player does not have a skills profile
+     */
+    public static boolean addStatModifier(Player player, String name, Stat stat, int value) {
+        PlayerStat playerStat = SkillLoader.playerStats.get(player.getUniqueId());
+        if (playerStat != null) {
+            playerStat.addModifier(new StatModifier(name, stat, value));
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Removes a stat modifier from a player
+     * @param player The player to remove from
+     * @param name The name of the stat modifier
+     * @return true if the operation was successful, false if the stat modifier was not found or the player does not have a skills profile
+     */
+    public static boolean removeStatModifier(Player player, String name) {
+        PlayerStat playerStat = SkillLoader.playerStats.get(player.getUniqueId());
+        if (playerStat != null) {
+            return playerStat.removeModifier(name);
+        }
+        return false;
+    }
+
 
 }
