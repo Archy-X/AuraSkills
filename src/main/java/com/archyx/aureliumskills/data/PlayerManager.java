66bf0f0334dbaf3ef9e54cd343977464928d3bad
Archy-X <andrewxiearchway@gmail.com>
2021-01-17 19:38:42 -0700

    Started player manager

---
+++ src/main/java/com/archyx/aureliumskills/data/PlayerManager.java
@@ -0,0 +1,10 @@
+package com.archyx.aureliumskills.data;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PlayerManager {
+
+    private ConcurrentHashMap<UUID, PlayerData> playerData;
+
+}
