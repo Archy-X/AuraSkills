80269dd027cfab0f1ff7de6568f12fb9bbed619b
Archy-X <archydevelopment@gmail.com>
2021-06-02 20:40:32 -0700

    Beta 1.0.3 Release

--- src/main/java/com/archyx/aureliumskills/AureliumSkills.java
+++ src/main/java/com/archyx/aureliumskills/AureliumSkills.java
@@ -108,7 +108,7 @@ public class AureliumSkills extends JavaPlugin {
 	private RegionManager regionManager;
 	private StatRegistry statRegistry;
 	private SkillRegistry skillRegistry;
-	private final long releaseTime = 1622502003315L;
+	private final long releaseTime = 1622691613056L;
 
 	public void onEnable() {
 		// Registries
