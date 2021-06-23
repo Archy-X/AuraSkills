4ffcddcb3c036853eda5cc81d5d65bc01f54971d
Archy-X <archydevelopment@gmail.com>
2021-06-22 18:06:59 -0700

    Add item key parsing for item loot

--- src/main/java/com/archyx/aureliumskills/loot/parser/ItemLootParser.java
+++ src/main/java/com/archyx/aureliumskills/loot/parser/ItemLootParser.java
@@ -44,89 +44,102 @@ public class ItemLootParser extends LootParser {
     }
 
     private ItemStack parseItem(Map<?, ?> map) {
-        // Parse material
-        String itemString = getString(map, "item");
-        Material material = Material.valueOf(itemString.toUpperCase(Locale.ROOT));
-        ItemStack item = new ItemStack(material);
+        if (map.containsKey("key")) { // Item key
+            return parseItemKey(map);
+        } else { // Regular item
+            String itemString = getString(map, "item");
+            Material material = Material.valueOf(itemString.toUpperCase(Locale.ROOT));
+            ItemStack item = new ItemStack(material);
 
-        ItemMeta meta = item.getItemMeta();
-        if (meta == null) return item;
-        // Display name and lore
-        if (map.containsKey("display_name")) {
-            String displayName = getString(map, "display_name");
-            meta.setDisplayName(displayName);
-            item.setItemMeta(meta);
-        }
-        if (map.containsKey("lore")) {
-            List<String> lore = getStringList(map, "lore");
-            meta.setLore(lore);
-            item.setItemMeta(meta);
-        }
-        // Enchantments
-        if (map.containsKey("enchantments")) {
-            List<String> enchantmentStrings = getStringList(map, "enchantments");
-            for (String enchantmentEntry : enchantmentStrings) {
-                String[] splitEntry = enchantmentEntry.split(" ");
-                String enchantmentName = splitEntry[0];
-                int level = 1;
-                if (splitEntry.length > 1) {
-                    level = NumberUtils.toInt(splitEntry[1], 1);
-                }
-                Optional<XEnchantment> xEnchantment = XEnchantment.matchXEnchantment(enchantmentName);
-                if (xEnchantment.isPresent()) {
-                    Enchantment enchantment = xEnchantment.get().parseEnchantment();
-                    if (enchantment != null) {
-                        if (item.getType() == Material.ENCHANTED_BOOK && meta instanceof EnchantmentStorageMeta) {
-                            EnchantmentStorageMeta esm = (EnchantmentStorageMeta) meta;
-                            esm.addStoredEnchant(enchantment, level, true);
-                            item.setItemMeta(esm);
+            ItemMeta meta = item.getItemMeta();
+            if (meta == null) return item;
+            // Display name and lore
+            if (map.containsKey("display_name")) {
+                String displayName = getString(map, "display_name");
+                meta.setDisplayName(displayName);
+                item.setItemMeta(meta);
+            }
+            if (map.containsKey("lore")) {
+                List<String> lore = getStringList(map, "lore");
+                meta.setLore(lore);
+                item.setItemMeta(meta);
+            }
+            // Enchantments
+            if (map.containsKey("enchantments")) {
+                List<String> enchantmentStrings = getStringList(map, "enchantments");
+                for (String enchantmentEntry : enchantmentStrings) {
+                    String[] splitEntry = enchantmentEntry.split(" ");
+                    String enchantmentName = splitEntry[0];
+                    int level = 1;
+                    if (splitEntry.length > 1) {
+                        level = NumberUtils.toInt(splitEntry[1], 1);
+                    }
+                    Optional<XEnchantment> xEnchantment = XEnchantment.matchXEnchantment(enchantmentName);
+                    if (xEnchantment.isPresent()) {
+                        Enchantment enchantment = xEnchantment.get().parseEnchantment();
+                        if (enchantment != null) {
+                            if (item.getType() == Material.ENCHANTED_BOOK && meta instanceof EnchantmentStorageMeta) {
+                                EnchantmentStorageMeta esm = (EnchantmentStorageMeta) meta;
+                                esm.addStoredEnchant(enchantment, level, true);
+                                item.setItemMeta(esm);
+                            } else {
+                                meta.addEnchant(enchantment, level, true);
+                                item.setItemMeta(meta);
+                            }
                         } else {
-                            meta.addEnchant(enchantment, level, true);
-                            item.setItemMeta(meta);
+                            throw new IllegalArgumentException("Invalid enchantment name " + enchantmentName);
                         }
                     } else {
                         throw new IllegalArgumentException("Invalid enchantment name " + enchantmentName);
                     }
-                } else {
-                    throw new IllegalArgumentException("Invalid enchantment name " + enchantmentName);
                 }
             }
-        }
-        // Potions
-        if (map.containsKey("potion_data")) {
-            Map<?, ?> potionDataMap = getMap(map, "potion_data");
-            PotionMeta potionMeta = (PotionMeta) item.getItemMeta();
-            PotionType potionType = PotionType.valueOf(getString(potionDataMap, "type"));
-            boolean extended = false;
-            if (potionDataMap.containsKey("extended")) {
-                extended = getBoolean(potionDataMap, "extended");
-            }
-            boolean upgraded = false;
-            if (potionDataMap.containsKey("upgraded")) {
-                upgraded = getBoolean(potionDataMap, "upgraded");
-            }
-            PotionData potionData = new PotionData(potionType, extended, upgraded);
-            potionMeta.setBasePotionData(potionData);
-            item.setItemMeta(potionMeta);
-        }
-        // Custom potion effects
-        if (map.containsKey("custom_effects")) {
-            PotionMeta potionMeta = (PotionMeta) item.getItemMeta();
-            for (Map<?, ?> effectMap : getMapList(map, "custom_effects")) {
-                String effectName = getString(effectMap, "type");
-                PotionEffectType type = PotionEffectType.getByName(effectName);
-                if (type != null) {
-                    int duration = getInt(effectMap, "duration");
-                    int amplifier = getInt(effectMap, "amplifier");
-                    potionMeta.addCustomEffect(new PotionEffect(type, duration, amplifier), true);
-                    potionMeta.setColor(type.getColor());
-                } else {
-                    throw new IllegalArgumentException("Invalid potion effect type " + effectName);
+            // Potions
+            if (map.containsKey("potion_data")) {
+                Map<?, ?> potionDataMap = getMap(map, "potion_data");
+                PotionMeta potionMeta = (PotionMeta) item.getItemMeta();
+                PotionType potionType = PotionType.valueOf(getString(potionDataMap, "type"));
+                boolean extended = false;
+                if (potionDataMap.containsKey("extended")) {
+                    extended = getBoolean(potionDataMap, "extended");
                 }
+                boolean upgraded = false;
+                if (potionDataMap.containsKey("upgraded")) {
+                    upgraded = getBoolean(potionDataMap, "upgraded");
+                }
+                PotionData potionData = new PotionData(potionType, extended, upgraded);
+                potionMeta.setBasePotionData(potionData);
+                item.setItemMeta(potionMeta);
             }
-            item.setItemMeta(potionMeta);
+            // Custom potion effects
+            if (map.containsKey("custom_effects")) {
+                PotionMeta potionMeta = (PotionMeta) item.getItemMeta();
+                for (Map<?, ?> effectMap : getMapList(map, "custom_effects")) {
+                    String effectName = getString(effectMap, "type");
+                    PotionEffectType type = PotionEffectType.getByName(effectName);
+                    if (type != null) {
+                        int duration = getInt(effectMap, "duration");
+                        int amplifier = getInt(effectMap, "amplifier");
+                        potionMeta.addCustomEffect(new PotionEffect(type, duration, amplifier), true);
+                        potionMeta.setColor(type.getColor());
+                    } else {
+                        throw new IllegalArgumentException("Invalid potion effect type " + effectName);
+                    }
+                }
+                item.setItemMeta(potionMeta);
+            }
+            return item;
         }
-        return item;
+    }
+
+    private ItemStack parseItemKey(Map<?, ?> map) {
+        String itemKey = getString(map, "key");
+        ItemStack item = plugin.getItemRegistry().getItem(itemKey);
+        if (item != null) {
+            return item;
+        } else {
+            throw new IllegalArgumentException("Item with key " + itemKey + " not found in item registry");
+        }
     }
 
     protected int[] parseAmount(Map<?, ?> map) {
