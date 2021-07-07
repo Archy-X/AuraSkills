36bc0e8e81ab3ffd7df4b628660db0b83029eca5
Archy-X <archydevelopment@gmail.com>
2021-07-06 21:45:00 -0700

    Improve Treecapitator algorithm

--- src/main/java/com/archyx/aureliumskills/mana/Treecapitator.java
+++ src/main/java/com/archyx/aureliumskills/mana/Treecapitator.java
@@ -6,12 +6,9 @@ import com.archyx.aureliumskills.data.PlayerData;
 import com.archyx.aureliumskills.lang.ManaAbilityMessage;
 import com.archyx.aureliumskills.skills.Skills;
 import com.archyx.aureliumskills.skills.foraging.ForagingSource;
-import com.cryptomorin.xseries.XMaterial;
-import org.bukkit.Material;
+import com.archyx.aureliumskills.util.block.BlockFaceUtil;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
-import org.bukkit.block.BlockState;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -57,18 +54,20 @@ public class Treecapitator extends ReadiedManaAbility {
     public void onBreak(BlockBreakEvent event) {
         if (event.isCancelled()) return;
         // Checks if block broken is log
-        Material blockMat = event.getBlock().getType();
-        if (blockMat.equals(XMaterial.OAK_LOG.parseMaterial()) || blockMat.equals(XMaterial.BIRCH_LOG.parseMaterial()) || blockMat.equals(XMaterial.SPRUCE_LOG.parseMaterial())
-                || blockMat.equals(XMaterial.JUNGLE_LOG.parseMaterial()) || blockMat.equals(XMaterial.ACACIA_LOG.parseMaterial()) || blockMat.equals(XMaterial.DARK_OAK_LOG.parseMaterial())) {
+        Block block = event.getBlock();
+        if (plugin.getRegionManager().isPlacedBlock(block)) {
+            return;
+        }
+        if (isTrunk(block) || block.getType().toString().contains("STRIPPED")) {
             Player player = event.getPlayer();
             if (isActivated(player)) {
-                breakTree(player, event.getBlock());
+                breakTree(player, block);
                 return;
             }
             if (isReady(player) && isHoldingMaterial(player) && hasEnoughMana(player)) {
                 if (hasEnoughMana(player)) {
                     activate(player);
-                    breakTree(player, event.getBlock());
+                    breakTree(player, block);
                 }
             }
         }
@@ -76,70 +75,128 @@ public class Treecapitator extends ReadiedManaAbility {
 
     public void breakTree(Player player, Block block) {
         if (plugin.getManaAbilityManager().isActivated(player.getUniqueId(), MAbility.TREECAPITATOR)) {
-            breakBlock(player, block.getState(), 0);
+            ForagingSource source = ForagingSource.getSource(block);
+            if (source != null) {
+                breakBlock(player, block, new TreecapitatorTree(block));
+            }
         }
     }
 
-    private void breakBlock(Player player, BlockState state, int num) {
-        if (num > 20) {
+    private void breakBlock(Player player, Block block, TreecapitatorTree tree) {
+        if (tree.getBlocksBroken() > tree.getMaxBlocks()) {
             return;
         }
-        BlockState above = state.getBlock().getRelative(BlockFace.UP).getState();
-        Material matAbove = above.getType();
-        if (matAbove.equals(XMaterial.OAK_LOG.parseMaterial()) || matAbove.equals(XMaterial.SPRUCE_LOG.parseMaterial()) || matAbove.equals(XMaterial.BIRCH_LOG.parseMaterial())
-                || matAbove.equals(XMaterial.JUNGLE_LOG.parseMaterial()) || matAbove.equals(XMaterial.ACACIA_LOG.parseMaterial()) || matAbove.equals(XMaterial.DARK_OAK_LOG.parseMaterial())) {
-            // Break log and give XP
-            ForagingSource source = ForagingSource.getSource(above.getBlock());
-            above.getBlock().breakNaturally();
-            plugin.getLeveler().addXp(player, Skills.FORAGING, getXp(player, source, Ability.FORAGER));
+        for (Block rel : BlockFaceUtil.getSurroundingBlocks(block)) {
+            boolean isTrunk = isTrunk(rel);
+            boolean isLeaf = isLeaf(rel);
+            if (!isTrunk && !isLeaf && !rel.getType().toString().equals("SHROOMLIGHT")) continue; // Check block is leaf or trunk
+            // Make sure block was not placed
+            if (plugin.getRegionManager().isPlacedBlock(rel)) {
+                continue;
+            }
+            ForagingSource source = ForagingSource.getSource(rel);
+            rel.breakNaturally();
+            tree.incrementBlocksBroken();
+            if (source != null) {
+                plugin.getLeveler().addXp(player, Skills.FORAGING, getXp(player, source, Ability.FORAGER));
+            }
+            // Continue breaking blocks
+            Block originalBlock = tree.getOriginalBlock();
+            if (rel.getX() > originalBlock.getX() + 6 || rel.getZ() > originalBlock.getZ() + 6 || rel.getY() > originalBlock.getY() + 31) {
+                return;
+            }
             new BukkitRunnable() {
                 @Override
                 public void run() {
-                    breakBlock(player, above, num + 1);
+                    breakBlock(player, rel, tree);
                 }
-            }.runTaskLater(plugin, 1L);
-        }
-        else {
-            checkLeaf(player, above);
-            checkLeaf(player, above.getBlock().getRelative(BlockFace.NORTH).getState());
-            checkLeaf(player, above.getBlock().getRelative(BlockFace.SOUTH).getState());
-            checkLeaf(player, above.getBlock().getRelative(BlockFace.EAST).getState());
-            checkLeaf(player, above.getBlock().getRelative(BlockFace.WEST).getState());
-
-        }
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.NORTH).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.EAST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.SOUTH).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.WEST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.NORTH_EAST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.NORTH_WEST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.SOUTH_EAST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.SOUTH_WEST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.NORTH_NORTH_EAST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.NORTH_NORTH_WEST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.EAST_NORTH_EAST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.EAST_SOUTH_EAST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.SOUTH_SOUTH_EAST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.SOUTH_SOUTH_WEST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.WEST_NORTH_WEST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.WEST_SOUTH_WEST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.NORTH).getRelative(BlockFace.NORTH).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.EAST).getRelative(BlockFace.EAST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.SOUTH).getRelative(BlockFace.SOUTH).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.WEST).getRelative(BlockFace.WEST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.NORTH_NORTH_EAST).getRelative(BlockFace.EAST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.SOUTH_SOUTH_EAST).getRelative(BlockFace.EAST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.NORTH_NORTH_WEST).getRelative(BlockFace.WEST).getState());
-        checkLeaf(player, state.getBlock().getRelative(BlockFace.SOUTH_SOUTH_WEST).getRelative(BlockFace.WEST).getState());
-    }
-
-    private void checkLeaf(Player player, BlockState state) {
-        Material material = state.getType();
-        if (material.equals(XMaterial.OAK_LEAVES.parseMaterial()) || material.equals(XMaterial.SPRUCE_LEAVES.parseMaterial()) || material.equals(XMaterial.BIRCH_LEAVES.parseMaterial())
-                || material.equals(XMaterial.JUNGLE_LEAVES.parseMaterial()) || material.equals(XMaterial.ACACIA_LEAVES.parseMaterial()) || material.equals(XMaterial.DARK_OAK_LEAVES.parseMaterial())) {
-            ForagingSource source = ForagingSource.getSource(state.getBlock());
-            state.getBlock().breakNaturally();
-            plugin.getLeveler().addXp(player, Skills.FORAGING, getXp(player, source, Ability.FORAGER));
+            }.runTaskLater(plugin, 1);
         }
     }
+
+    private boolean isTrunk(Block block) {
+        ForagingSource source = ForagingSource.getSource(block);
+        if (source != null) {
+            return source.isTrunk();
+        }
+        return false;
+    }
+
+    private boolean isLeaf(Block block) {
+        ForagingSource source = ForagingSource.getSource(block);
+        if (source != null) {
+            return source.isLeaf();
+        }
+        return false;
+    }
+
+    private static class TreecapitatorTree {
+
+        private final Block originalBlock;
+        private int blocksBroken;
+        private int maxBlocks;
+
+        public TreecapitatorTree(Block originalBlock) {
+            this.originalBlock = originalBlock;
+            setMaxBlocks();
+        }
+
+        public Block getOriginalBlock() {
+            return originalBlock;
+        }
+
+        public int getBlocksBroken() {
+            return blocksBroken;
+        }
+
+        public void incrementBlocksBroken() {
+            blocksBroken++;
+        }
+
+        public int getMaxBlocks() {
+            return maxBlocks;
+        }
+
+        private void setMaxBlocks() {
+            ForagingSource source = ForagingSource.getSource(originalBlock);
+            String matName = originalBlock.getType().toString();
+            if (source == null && matName.contains("STRIPPED")) {
+                String[] woodNames = new String[] {"OAK", "SPRUCE", "BIRCH", "JUNGLE", "ACACIA", "DARK_OAK", "CRIMSON", "WARPED"};
+                for (String woodName : woodNames) {
+                    if (matName.contains(woodName)) {
+                        if (woodName.equals("CRIMSON") || woodName.equals("WARPED")) {
+                            source = ForagingSource.valueOf(woodName + "_STEM");
+                        } else {
+                            source = ForagingSource.valueOf(woodName + "_LOG");
+                        }
+                        break;
+                    }
+                }
+            }
+            if (source != null) {
+                switch (source) {
+                    case OAK_LOG:
+                    case BIRCH_LOG:
+                    case ACACIA_LOG:
+                        maxBlocks = 100;
+                        break;
+                    case SPRUCE_LOG:
+                    case CRIMSON_STEM:
+                    case WARPED_STEM:
+                        maxBlocks = 125;
+                        break;
+                    case JUNGLE_LOG:
+                        maxBlocks = 200;
+                        break;
+                    case DARK_OAK_LOG:
+                        maxBlocks = 150;
+                        break;
+                }
+            } else {
+                maxBlocks = 100;
+            }
+        }
+
+    }
+
 }
