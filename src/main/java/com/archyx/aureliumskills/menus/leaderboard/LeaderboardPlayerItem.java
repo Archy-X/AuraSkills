a2d312bf4354bbb0762bd3c28aa1bc9690cc562b
Archy-X <archydevelopment@gmail.com>
2021-09-09 13:40:57 -0700

    Fix leaderboard menu not using actual leaderboard players

--- src/main/java/com/archyx/aureliumskills/menus/leaderboard/LeaderboardPlayerItem.java
+++ src/main/java/com/archyx/aureliumskills/menus/leaderboard/LeaderboardPlayerItem.java
@@ -4,17 +4,22 @@ import com.archyx.aureliumskills.AureliumSkills;
 import com.archyx.aureliumskills.data.PlayerData;
 import com.archyx.aureliumskills.lang.Lang;
 import com.archyx.aureliumskills.lang.MenuMessage;
+import com.archyx.aureliumskills.leaderboard.SkillValue;
 import com.archyx.aureliumskills.menus.common.AbstractItem;
 import com.archyx.aureliumskills.skills.Skill;
 import com.archyx.aureliumskills.util.text.TextUtil;
 import com.archyx.slate.item.provider.PlaceholderType;
 import com.archyx.slate.item.provider.TemplateItemProvider;
 import com.archyx.slate.menu.ActiveMenu;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Set;
+import java.util.UUID;
 
 public class LeaderboardPlayerItem extends AbstractItem implements TemplateItemProvider<Integer> {
 
@@ -30,15 +35,19 @@ public class LeaderboardPlayerItem extends AbstractItem implements TemplateItemP
     @Override
     public String onPlaceholderReplace(String placeholder, Player player, ActiveMenu activeMenu, PlaceholderType placeholderType, Integer place) {
         Locale locale = plugin.getLang().getLocale(player);
+        Skill skill = (Skill) activeMenu.getProperty("skill");
         switch (placeholder) {
             case "place":
                 return String.valueOf(place);
             case "player":
-                return player.getName();
+                SkillValue skillValue = plugin.getLeaderboardManager().getLeaderboard(skill, place, 1).get(0);
+                UUID id = skillValue.getId();
+                return Bukkit.getOfflinePlayer(id).getName();
             case "skill_level":
-                PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
+                SkillValue value = plugin.getLeaderboardManager().getLeaderboard(skill, place, 1).get(0);
+                UUID uuid = value.getId();
+                PlayerData playerData = plugin.getPlayerManager().getPlayerData(uuid);
                 if (playerData != null) {
-                    Skill skill = (Skill) activeMenu.getProperty("skill");
                     return TextUtil.replace(Lang.getMessage(MenuMessage.SKILL_LEVEL, locale),
                             "{level}", String.valueOf(playerData.getSkillLevel(skill)));
                 }
@@ -53,5 +62,18 @@ public class LeaderboardPlayerItem extends AbstractItem implements TemplateItemP
             places.add(i);
         }
         return places;
+    }
+
+    @Override
+    public ItemStack onItemModify(ItemStack baseItem, Player player, ActiveMenu activeMenu, Integer place) {
+        Skill skill = (Skill) activeMenu.getProperty("skill");
+        SkillValue skillValue = plugin.getLeaderboardManager().getLeaderboard(skill, place, 1).get(0);
+        UUID id = skillValue.getId();
+        if (baseItem.getItemMeta() instanceof SkullMeta) {
+            SkullMeta meta = (SkullMeta) baseItem.getItemMeta();
+            meta.setOwningPlayer(Bukkit.getOfflinePlayer(id));
+            baseItem.setItemMeta(meta);
+        }
+        return baseItem;
     }
 }
