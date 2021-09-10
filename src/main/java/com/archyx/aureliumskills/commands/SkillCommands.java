e9fcf21a3e225469435575b4d69f4d382c34368e
Archy-X <archydevelopment@gmail.com>
2021-09-10 16:09:03 -0700

    Change skill commands to open new level progression menu

--- src/main/java/com/archyx/aureliumskills/commands/SkillCommands.java
+++ src/main/java/com/archyx/aureliumskills/commands/SkillCommands.java
@@ -4,281 +4,268 @@ import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.Default;
 import com.archyx.aureliumskills.AureliumSkills;
+import com.archyx.aureliumskills.configuration.OptionL;
 import com.archyx.aureliumskills.data.PlayerData;
-import com.archyx.aureliumskills.menu.LevelProgressionMenu;
-import com.archyx.aureliumskills.menu.SkillsMenu;
+import com.archyx.aureliumskills.skills.Skill;
 import com.archyx.aureliumskills.skills.Skills;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SkillCommands {
 
-    @CommandAlias("farming")
-    public static class FarmingCommand extends BaseCommand {
-
-        private final AureliumSkills plugin;
-
-        public FarmingCommand(AureliumSkills plugin) {
+    public static class SkillCommand extends BaseCommand {
+        
+        protected final AureliumSkills plugin;
+        protected final Skill skill;
+        
+        public SkillCommand(AureliumSkills plugin, Skill skill) {
             this.plugin = plugin;
+            this.skill = skill;
+        }
+        
+        protected Map<String, Object> getProperties() {
+            Map<String, Object> properties = new HashMap<>();
+            properties.put("skill", skill);
+            properties.put("items_per_page", 24);
+            properties.put("previous_menu", "skills");
+            return properties;
+        }
+
+        protected int getPage(PlayerData playerData) {
+            int page = (playerData.getSkillLevel(skill) - 2) / 24;
+            int maxLevelPage = (OptionL.getMaxLevel(skill) - 2) / 24;
+            if (page > maxLevelPage) {
+                page = maxLevelPage;
+            }
+            return page;
+        }
+        
+    }
+
+    @CommandAlias("farming")
+    public static class FarmingCommand extends SkillCommand {
+        
+        public FarmingCommand(AureliumSkills plugin) {
+            super(plugin, Skills.FARMING);
         }
 
         @Default
         public void onCommand(Player player) {
             PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
             if (playerData != null) {
-                int page = SkillsMenu.getPage(Skills.FARMING, playerData);
-                LevelProgressionMenu.getInventory(player, Skills.FARMING, page, plugin).open(player, page);
+                plugin.getSlate().getMenuManager().openMenu(player, "level_progression", getProperties(), getPage(playerData));
             }
         }
     }
     @CommandAlias("foraging")
-    public static class ForagingCommand extends BaseCommand {
-
-        private final AureliumSkills plugin;
+    public static class ForagingCommand extends SkillCommand {
 
         public ForagingCommand(AureliumSkills plugin) {
-            this.plugin = plugin;
+            super(plugin, Skills.FORAGING);
         }
 
         @Default
         public void onCommand(Player player) {
             PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
             if (playerData != null) {
-                int page = SkillsMenu.getPage(Skills.FORAGING, playerData);
-                LevelProgressionMenu.getInventory(player, Skills.FORAGING, page, plugin).open(player, page);
+                plugin.getSlate().getMenuManager().openMenu(player, "level_progression", getProperties(), getPage(playerData));
             }
         }
     }
     @CommandAlias("mining")
-    public static class MiningCommand extends BaseCommand {
-
-        private final AureliumSkills plugin;
+    public static class MiningCommand extends SkillCommand {
 
         public MiningCommand(AureliumSkills plugin) {
-            this.plugin = plugin;
+            super(plugin, Skills.MINING);
         }
 
         @Default
         public void onCommand(Player player) {
             PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
             if (playerData != null) {
-                int page = SkillsMenu.getPage(Skills.MINING, playerData);
-                LevelProgressionMenu.getInventory(player, Skills.MINING, page, plugin).open(player, page);
+                plugin.getSlate().getMenuManager().openMenu(player, "level_progression", getProperties(), getPage(playerData));
             }
         }
     }
     @CommandAlias("fishing")
-    public static class FishingCommand extends BaseCommand {
-
-        private final AureliumSkills plugin;
+    public static class FishingCommand extends SkillCommand {
 
         public FishingCommand(AureliumSkills plugin) {
-            this.plugin = plugin;
+            super(plugin, Skills.FISHING);
         }
 
         @Default
         public void onCommand(Player player) {
             PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
             if (playerData != null) {
-                int page = SkillsMenu.getPage(Skills.FISHING, playerData);
-                LevelProgressionMenu.getInventory(player, Skills.FISHING, page, plugin).open(player, page);
+                plugin.getSlate().getMenuManager().openMenu(player, "level_progression", getProperties(), getPage(playerData));
             }
         }
     }
     @CommandAlias("excavation")
-    public static class ExcavationCommand extends BaseCommand {
-
-        private final AureliumSkills plugin;
+    public static class ExcavationCommand extends SkillCommand {
 
         public ExcavationCommand(AureliumSkills plugin) {
-            this.plugin = plugin;
+            super(plugin, Skills.EXCAVATION);
         }
 
         @Default
         public void onCommand(Player player) {
             PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
             if (playerData != null) {
-                int page = SkillsMenu.getPage(Skills.EXCAVATION, playerData);
-                LevelProgressionMenu.getInventory(player, Skills.EXCAVATION, page, plugin).open(player, page);
+                plugin.getSlate().getMenuManager().openMenu(player, "level_progression", getProperties(), getPage(playerData));
             }
         }
     }
     @CommandAlias("archery")
-    public static class ArcheryCommand extends BaseCommand {
-
-        private final AureliumSkills plugin;
+    public static class ArcheryCommand extends SkillCommand {
 
         public ArcheryCommand(AureliumSkills plugin) {
-            this.plugin = plugin;
+            super(plugin, Skills.ARCHERY);
         }
         
         @Default
         public void onCommand(Player player) {
             PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
             if (playerData != null) {
-                int page = SkillsMenu.getPage(Skills.ARCHERY, playerData);
-                LevelProgressionMenu.getInventory(player, Skills.ARCHERY, page, plugin).open(player, page);
+                plugin.getSlate().getMenuManager().openMenu(player, "level_progression", getProperties(), getPage(playerData));
             }
         }
     }
     @CommandAlias("defense")
-    public static class DefenseCommand extends BaseCommand {
-
-        private final AureliumSkills plugin;
+    public static class DefenseCommand extends SkillCommand {
 
         public DefenseCommand(AureliumSkills plugin) {
-            this.plugin = plugin;
+            super(plugin, Skills.DEFENSE);
         }
         
         @Default
         public void onCommand(Player player) {
             PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
             if (playerData != null) {
-                int page = SkillsMenu.getPage(Skills.DEFENSE, playerData);
-                LevelProgressionMenu.getInventory(player, Skills.DEFENSE, page, plugin).open(player, page);
+                plugin.getSlate().getMenuManager().openMenu(player, "level_progression", getProperties(), getPage(playerData));
             }
         }
     }
     @CommandAlias("fighting")
-    public static class FightingCommand extends BaseCommand {
-
-        private final AureliumSkills plugin;
+    public static class FightingCommand extends SkillCommand {
 
         public FightingCommand(AureliumSkills plugin) {
-            this.plugin = plugin;
+            super(plugin, Skills.FIGHTING);
         }
         
         @Default
         public void onCommand(Player player) {
             PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
             if (playerData != null) {
-                int page = SkillsMenu.getPage(Skills.FIGHTING, playerData);
-                LevelProgressionMenu.getInventory(player, Skills.FIGHTING, page, plugin).open(player, page);
+                plugin.getSlate().getMenuManager().openMenu(player, "level_progression", getProperties(), getPage(playerData));
             }
         }
     }
     @CommandAlias("endurance")
-    public static class EnduranceCommand extends BaseCommand {
-
-        private final AureliumSkills plugin;
+    public static class EnduranceCommand extends SkillCommand {
 
         public EnduranceCommand(AureliumSkills plugin) {
-            this.plugin = plugin;
+            super(plugin, Skills.ENDURANCE);
         }
         
         @Default
         public void onCommand(Player player) {
             PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
             if (playerData != null) {
-                int page = SkillsMenu.getPage(Skills.ENDURANCE, playerData);
-                LevelProgressionMenu.getInventory(player, Skills.ENDURANCE, page, plugin).open(player, page);
+                plugin.getSlate().getMenuManager().openMenu(player, "level_progression", getProperties(), getPage(playerData));
             }
         }
     }
     @CommandAlias("agility")
-    public static class AgilityCommand extends BaseCommand {
-
-        private final AureliumSkills plugin;
+    public static class AgilityCommand extends SkillCommand {
 
         public AgilityCommand(AureliumSkills plugin) {
-            this.plugin = plugin;
+            super(plugin, Skills.AGILITY);
         }
         
         @Default
         public void onCommand(Player player) {
             PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
             if (playerData != null) {
-                int page = SkillsMenu.getPage(Skills.AGILITY, playerData);
-                LevelProgressionMenu.getInventory(player, Skills.AGILITY, page, plugin).open(player, page);
+                plugin.getSlate().getMenuManager().openMenu(player, "level_progression", getProperties(), getPage(playerData));
             }
         }
     }
     @CommandAlias("alchemy")
-    public static class AlchemyCommand extends BaseCommand {
-
-        private final AureliumSkills plugin;
+    public static class AlchemyCommand extends SkillCommand {
 
         public AlchemyCommand(AureliumSkills plugin) {
-            this.plugin = plugin;
+            super(plugin, Skills.ALCHEMY);
         }
 
         @Default
         public void onCommand(Player player) {
             PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
             if (playerData != null) {
-                int page = SkillsMenu.getPage(Skills.ALCHEMY, playerData);
-                LevelProgressionMenu.getInventory(player, Skills.ALCHEMY, page, plugin).open(player, page);
+                plugin.getSlate().getMenuManager().openMenu(player, "level_progression", getProperties(), getPage(playerData));
             }
         }
     }
     @CommandAlias("enchanting")
-    public static class EnchantingCommand extends BaseCommand {
-
-        private final AureliumSkills plugin;
+    public static class EnchantingCommand extends SkillCommand {
 
         public EnchantingCommand(AureliumSkills plugin) {
-            this.plugin = plugin;
+            super(plugin, Skills.ENCHANTING);
         }
 
         @Default
         public void onCommand(Player player) {
             PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
             if (playerData != null) {
-                int page = SkillsMenu.getPage(Skills.ENCHANTING, playerData);
-                LevelProgressionMenu.getInventory(player, Skills.ENCHANTING, page, plugin).open(player, page);
+                plugin.getSlate().getMenuManager().openMenu(player, "level_progression", getProperties(), getPage(playerData));
             }
         }
     }
     @CommandAlias("sorcery")
-    public static class SorceryCommand extends BaseCommand {
-
-        private final AureliumSkills plugin;
+    public static class SorceryCommand extends SkillCommand {
 
         public SorceryCommand(AureliumSkills plugin) {
-            this.plugin = plugin;
+            super(plugin, Skills.SORCERY);
         }
 
         @Default
         public void onCommand(Player player) {
             PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
             if (playerData != null) {
-                int page = SkillsMenu.getPage(Skills.SORCERY, playerData);
-                LevelProgressionMenu.getInventory(player, Skills.SORCERY, page, plugin).open(player, page);
+                plugin.getSlate().getMenuManager().openMenu(player, "level_progression", getProperties(), getPage(playerData));
             }
         }
     }
     @CommandAlias("healing")
-    public static class HealingCommand extends BaseCommand {
-
-        private final AureliumSkills plugin;
+    public static class HealingCommand extends SkillCommand {
 
         public HealingCommand(AureliumSkills plugin) {
-            this.plugin = plugin;
+            super(plugin, Skills.HEALING);
         }
 
         @Default
         public void onCommand(Player player) {
             PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
             if (playerData != null) {
-                int page = SkillsMenu.getPage(Skills.HEALING, playerData);
-                LevelProgressionMenu.getInventory(player, Skills.HEALING, page, plugin).open(player, page);
+                plugin.getSlate().getMenuManager().openMenu(player, "level_progression", getProperties(), getPage(playerData));
             }
         }
     }
     @CommandAlias("forging")
-    public static class ForgingCommand extends BaseCommand {
-
-        private final AureliumSkills plugin;
+    public static class ForgingCommand extends SkillCommand {
 
         public ForgingCommand(AureliumSkills plugin) {
-            this.plugin = plugin;
+            super(plugin, Skills.FORGING);
         }
 
         @Default
         public void onCommand(Player player) {
             PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
             if (playerData != null) {
-                int page = SkillsMenu.getPage(Skills.FORGING, playerData);
-                LevelProgressionMenu.getInventory(player, Skills.FORGING, page, plugin).open(player, page);
+                plugin.getSlate().getMenuManager().openMenu(player, "level_progression", getProperties(), getPage(playerData));
             }
         }
     }
