324f25099240bb557a1d20c232b0a186749e05d8
Archy-X <archydevelopment@gmail.com>
2021-09-11 23:06:08 -0700

    Optimize SourceRegistry#values by caching on registration

--- src/main/java/com/archyx/aureliumskills/source/SourceRegistry.java
+++ src/main/java/com/archyx/aureliumskills/source/SourceRegistry.java
@@ -12,56 +12,51 @@ import java.util.*;
 public class SourceRegistry {
 
     private final Map<Skill, Class<?>> registry;
+    private final Map<Skill, Source[]> sources;
 
     public SourceRegistry() {
         registry = new HashMap<>();
+        sources = new HashMap<>();
         try {
             for (Skill skill : Skills.values()) {
                 String className = StringUtils.capitalize(skill.toString().toLowerCase(Locale.ROOT)) + "Source";
                 Class<?> sourceClass = Class.forName("com.archyx.aureliumskills.skills." + skill.toString().toLowerCase(Locale.ROOT) + "." + className);
-                registry.put(skill, sourceClass);
+                register(skill, sourceClass);
             }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
+    public Class<?> getSourceClass(Skill skill) {
+        return registry.get(skill);
+    }
+
     /**
      * The registered class must be an enum or a class that has a values method that returns an array of Source objects.
      * Any class must implement
      */
-    public void register(Skill skill, Class<? extends Source> sourceClass) {
+    public void register(Skill skill, Class<?> sourceClass) {
         registry.put(skill, sourceClass);
+        try {
+            Method method = sourceClass.getMethod("values");
+            Object object = method.invoke(null);
+            if (object instanceof Source[]) {
+                sources.put(skill, (Source[]) object);
+            }
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException | IllegalArgumentException e) {
+            e.printStackTrace();
+        }
     }
 
     public Source[] values(Skill skill) {
-        Class<?> sourceClass = registry.get(skill);
-        if (sourceClass != null) {
-            try {
-                Method method = sourceClass.getMethod("values");
-                Object object = method.invoke(null);
-                if (object instanceof Source[]) {
-                    return (Source[]) object;
-                }
-            } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException | IllegalArgumentException e) {
-                e.printStackTrace();
-            }
-        }
-        return new Source[0];
+        return sources.get(skill);
     }
 
     public Set<Source> values() {
         Set<Source> sourceSet = new HashSet<>();
-        for (Class<?> sourceClass : registry.values()) {
-            try {
-                Method method = sourceClass.getMethod("values");
-                Object object = method.invoke(null);
-                if (object instanceof Source[]) {
-                    sourceSet.addAll(Arrays.asList((Source[]) object));
-                }
-            } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException | IllegalArgumentException e) {
-                e.printStackTrace();
-            }
+        for (Source[] skillSources : sources.values()) {
+            sourceSet.addAll(Arrays.asList(skillSources));
         }
         return sourceSet;
     }
