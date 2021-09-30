ce96f512d50635955d7b6fa5c4e698d6c4bfa7c2
Archy-X <archydevelopment@gmail.com>
2021-09-29 20:58:01 -0700

    Change NumberFormat RoundingMode to HALF_UP

--- src/main/java/com/archyx/aureliumskills/util/math/NumberUtil.java
+++ src/main/java/com/archyx/aureliumskills/util/math/NumberUtil.java
@@ -1,5 +1,6 @@
 package com.archyx.aureliumskills.util.math;
 
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
@@ -8,6 +9,12 @@ public class NumberUtil {
     private static final NumberFormat zero = new DecimalFormat("#");
     private static final NumberFormat one = new DecimalFormat("#.#");
     private static final NumberFormat two = new DecimalFormat("#.##");
+
+    static {
+        zero.setRoundingMode(RoundingMode.HALF_UP);
+        one.setRoundingMode(RoundingMode.HALF_UP);
+        two.setRoundingMode(RoundingMode.HALF_UP);
+    }
 
     public static String format0(double input) {
         return zero.format(input);
