38f81c8c3e060736ee01ce466c557e03289673cf
Archy-X <archydevelopment@gmail.com>
2022-05-10 16:55:50 -0700

    Remove abilities item in Sorcery level progression menu

--- src/main/java/com/archyx/aureliumskills/menus/levelprogression/AbilitiesItem.java
+++ src/main/java/com/archyx/aureliumskills/menus/levelprogression/AbilitiesItem.java
@@ -1,10 +1,12 @@
 package com.archyx.aureliumskills.menus.levelprogression;
 
 import com.archyx.aureliumskills.AureliumSkills;
+import com.archyx.aureliumskills.ability.Ability;
 import com.archyx.aureliumskills.lang.Lang;
 import com.archyx.aureliumskills.lang.MenuMessage;
 import com.archyx.aureliumskills.menus.common.AbstractItem;
 import com.archyx.aureliumskills.skills.Skill;
+import com.archyx.aureliumskills.skills.Skills;
 import com.archyx.aureliumskills.util.text.TextUtil;
 import com.archyx.slate.item.provider.PlaceholderType;
 import com.archyx.slate.item.provider.SingleItemProvider;
@@ -17,6 +19,7 @@ import org.bukkit.inventory.ItemStack;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.function.Supplier;
 
 public class AbilitiesItem extends AbstractItem implements SingleItemProvider {
 
@@ -45,5 +48,26 @@ public class AbilitiesItem extends AbstractItem implements SingleItemProvider {
         properties.put("skill", activeMenu.getProperty("skill"));
         properties.put("previous_menu", "level_progression");
         plugin.getMenuManager().openMenu(player, "abilities", properties);
+    }
+
+    @Override
+    public ItemStack onItemModify(ItemStack baseItem, Player player, ActiveMenu activeMenu) {
+        Skill skill = (Skill) activeMenu.getProperty("skill");
+        if (skill == Skills.SORCERY) { // Disable for sorcery abilities REMOVE ONCE SORCERY ABILITIES ARE ADDED
+            return null;
+        }
+        // Check if the skill has an enabled ability
+        boolean hasEnabledAbility = false;
+        for (Supplier<Ability> abilitySupplier : skill.getAbilities()) {
+           if (plugin.getAbilityManager().isEnabled(abilitySupplier.get())) {
+                hasEnabledAbility = true;
+                break;
+            }
+        }
+        if (hasEnabledAbility) {
+            return baseItem;
+        } else {
+            return null; // Don't show item if no abilities are enabled
+        }
     }
 }
