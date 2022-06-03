83e8cbe120dec2942f73e467420d382dce348627
Archy-X <archydevelopment@gmail.com>
2022-06-02 18:46:26 -0700

    Beta 1.3.4 Release

--- src/main/java/com/archyx/aureliumskills/util/version/ReleaseData.java
+++ src/main/java/com/archyx/aureliumskills/util/version/ReleaseData.java
@@ -2,6 +2,6 @@ package com.archyx.aureliumskills.util.version;
 
 public class ReleaseData {
 
-    public static final long RELEASE_TIME = 1653943833376L;
+    public static final long RELEASE_TIME = 1654220755586L;
 
 }
