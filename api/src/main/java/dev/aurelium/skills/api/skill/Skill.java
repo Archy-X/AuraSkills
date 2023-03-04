27e4a6c1ff43477cd0793e463398058bddd0c054
Archy-X <archydevelopment@gmail.com>
2023-03-03 22:41:51 -0500

    Continue SkillsPlayer API

--- api/src/main/java/dev/aurelium/skills/api/skill/Skill.java
+++ api/src/main/java/dev/aurelium/skills/api/skill/Skill.java
@@ -1,7 +1,13 @@
 package dev.aurelium.skills.api.skill;
 
+import java.util.Locale;
+
 public interface Skill {
 
+    String getId();
 
+    String getDisplayName(Locale locale);
+
+    String getDescription(Locale locale);
 
 }
