3b4acc9d14261b83882af5f3819d443d5166f1df
Archy-X <archydevelopment@gmail.com>
2023-03-03 23:32:31 -0500

    Fix API compile errors

--- api/src/main/java/dev/aurelium/skills/api/skill/Skills.java
+++ api/src/main/java/dev/aurelium/skills/api/skill/Skills.java
@@ -1,5 +1,7 @@
 package dev.aurelium.skills.api.skill;
 
+import java.util.Locale;
+
 public enum Skills implements Skill {
 
     FARMING,
@@ -16,6 +18,20 @@ public enum Skills implements Skill {
     ENCHANTING,
     SORCERY,
     HEALING,
-    FORGING
+    FORGING;
 
+    @Override
+    public String getId() {
+        return toString().toLowerCase(Locale.ROOT);
+    }
+
+    @Override
+    public String getDisplayName(Locale locale) {
+        return null;
+    }
+
+    @Override
+    public String getDescription(Locale locale) {
+        return null;
+    }
 }
