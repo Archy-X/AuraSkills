ddc43ca54899de4722624679c919927dc8a05175
Archy-X <archydevelopment@gmail.com>
2023-03-06 00:06:26 -0500

    Refactor Skill back into an interface, add more config API methods

--- api/src/main/java/dev/aurelium/skills/api/skill/Skills.java
+++ api/src/main/java/dev/aurelium/skills/api/skill/Skills.java
@@ -1,21 +1,34 @@
 package dev.aurelium.skills.api.skill;
 
-public class Skills {
+import dev.aurelium.skills.api.util.NamespacedId;
 
-    public static final Skill FARMING = new SkillWrapper("farming");
-    public static final Skill FORAGING = new SkillWrapper("foraging");
-    public static final Skill MINING = new SkillWrapper("mining");
-    public static final Skill FISHING = new SkillWrapper("fishing");
-    public static final Skill EXCAVATION = new SkillWrapper("excavation");
-    public static final Skill ARCHERY = new SkillWrapper("archery");
-    public static final Skill FIGHTING = new SkillWrapper("fighting");
-    public static final Skill DEFENSE = new SkillWrapper("defense");
-    public static final Skill AGILITY = new SkillWrapper("agility");
-    public static final Skill ENDURANCE = new SkillWrapper("endurance");
-    public static final Skill ALCHEMY = new SkillWrapper("alchemy");
-    public static final Skill ENCHANTING = new SkillWrapper("enchanting");
-    public static final Skill SORCERY = new SkillWrapper("sorcery");
-    public static final Skill HEALING = new SkillWrapper("healing");
-    public static final Skill FORGING = new SkillWrapper("forging");
+public enum Skills implements Skill {
+
+    FARMING("farming"),
+    FORAGING("foraging"),
+    MINING("mining"),
+    FISHING("fishing"),
+    EXCAVATION("excavation"),
+    ARCHERY("archery"),
+    FIGHTING("fighting"),
+    DEFENSE("defense"),
+    AGILITY("agility"),
+    ENDURANCE("endurance"),
+    ALCHEMY("alchemy"),
+    ENCHANTING("enchanting"),
+    SORCERY("sorcery"),
+    HEALING("healing"),
+    FORGING("forging");
+
+    private final NamespacedId id;
+
+    Skills(String id) {
+        this.id = new NamespacedId("aureliumskills", id);
+    }
+
+    @Override
+    public NamespacedId getId() {
+        return id;
+    }
 
 }
