e812cd6f32868b3c0a41cc28ffce43094470f549
Archy-X <archydevelopment@gmail.com>
2023-04-12 18:41:52 -0400

    Move API implementation from bukkit to common module

--- common/src/main/java/dev/aurelium/skills/common/message/MessageProvider.java
+++ common/src/main/java/dev/aurelium/skills/common/message/MessageProvider.java
@@ -1,10 +1,21 @@
 package dev.aurelium.skills.common.message;
 
+import dev.aurelium.skills.api.skill.Skill;
+import dev.aurelium.skills.api.stat.Stat;
+
 import java.util.Locale;
 
 public interface MessageProvider {
 
     String get(MessageKey key, Locale locale);
+
+    String getSkillDisplayName(Locale locale, Skill skill);
+
+    String getSkillDescription(Locale locale, Skill skill);
+
+    String getStatDisplayName(Locale locale, Stat stat);
+
+    String getStatDescription(Locale locale, Stat stat);
 
     Locale getDefaultLanguage();
 
