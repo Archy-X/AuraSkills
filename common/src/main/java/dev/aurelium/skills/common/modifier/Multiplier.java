82afe38f51b4f1c291372e4290352d04622dad06
Archy-X <archydevelopment@gmail.com>
2023-05-01 14:36:14 -0400

    Remove equals method from Multiplier

--- common/src/main/java/dev/aurelium/skills/common/modifier/Multiplier.java
+++ common/src/main/java/dev/aurelium/skills/common/modifier/Multiplier.java
@@ -2,20 +2,10 @@ package dev.aurelium.skills.common.modifier;
 
 import dev.aurelium.skills.api.skill.Skill;
 
-import java.util.Objects;
-
 public record Multiplier(String name, Skill skill, double value) {
 
     public boolean isGlobal() {
         return skill == null;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Multiplier that = (Multiplier) o;
-        return Double.compare(that.value, value) == 0 && Objects.equals(name, that.name) && Objects.equals(skill, that.skill);
     }
 
 }
