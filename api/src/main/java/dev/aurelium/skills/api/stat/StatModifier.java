d5b5e2b2917947c851cc93e894f491f799a8cbfd
Archy-X <archydevelopment@gmail.com>
2023-05-01 14:39:33 -0400

    Convert StatModifier in api to a record

--- api/src/main/java/dev/aurelium/skills/api/stat/StatModifier.java
+++ api/src/main/java/dev/aurelium/skills/api/stat/StatModifier.java
@@ -1,26 +1,5 @@
 package dev.aurelium.skills.api.stat;
 
-public class StatModifier {
+public record StatModifier(String name, Stat stat, double value) {
 
-    private final String name;
-    private final Stat stat;
-    private final double value;
-
-    public StatModifier(String name, Stat stat, double value) {
-        this.name = name;
-        this.stat = stat;
-        this.value = value;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Stat getStat() {
-        return stat;
-    }
-
-    public double getValue() {
-        return value;
-    }
 }
