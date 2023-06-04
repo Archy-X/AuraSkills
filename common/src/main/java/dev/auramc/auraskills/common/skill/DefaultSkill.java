d3ed92eb7db6848e29ed2fa488718fbbd2ce416a
Archy-X <archydevelopment@gmail.com>
2023-06-03 20:22:06 -0700

    Register sources into skills

--- common/src/main/java/dev/auramc/auraskills/common/skill/DefaultSkill.java
+++ common/src/main/java/dev/auramc/auraskills/common/skill/DefaultSkill.java
@@ -4,8 +4,9 @@ import com.google.common.collect.ImmutableList;
 import dev.auramc.auraskills.api.ability.Ability;
 import dev.auramc.auraskills.api.mana.ManaAbility;
 import dev.auramc.auraskills.api.skill.Skill;
+import dev.auramc.auraskills.common.source.Source;
 
 public record DefaultSkill(Skill skill, ImmutableList<Ability> abilities,
-                           ManaAbility manaAbility) implements SkillProperties {
+                           ManaAbility manaAbility, ImmutableList<Source> sources) implements SkillProperties {
 
 }
