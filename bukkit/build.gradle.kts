e07ab625393fc8ed66e416b68f1dfefa28b80375
Archy-X <archydevelopment@gmail.com>
2023-08-23 19:39:55 -0700

    Fix console spam from last commit

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation("org.bstats:bstats-bukkit:3.0.0")
     implementation("org.jetbrains:annotations:23.0.0")
     implementation("com.udojava:EvalEx:2.7")
-    implementation("com.github.Archy-X:Slate:6500eb1c78") {
+    implementation("com.github.Archy-X:Slate:d2a43587ad") {
         exclude("org.spigotmc", "spigot-api")
     }
     implementation("com.github.Archy-X:LootManager:60d109fdde")
