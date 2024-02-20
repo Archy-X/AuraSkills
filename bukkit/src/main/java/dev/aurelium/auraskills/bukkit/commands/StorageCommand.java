1d320e91bd59abb8027e312bb5649c901c189596
Archy-X <archydevelopment@gmail.com>
2024-02-20 12:43:11 -0500

    Add removeallmodifiers command

--- bukkit/src/main/java/dev/aurelium/auraskills/bukkit/commands/StorageCommand.java
+++ bukkit/src/main/java/dev/aurelium/auraskills/bukkit/commands/StorageCommand.java
@@ -13,6 +13,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.command.RemoteConsoleCommandSender;
 
+import java.util.HashMap;
 import java.util.Map.Entry;
 
 @CommandAlias("%skills_alias")
@@ -26,7 +27,7 @@ public class StorageCommand extends BaseCommand {
     }
 
     @Subcommand("mergeskills")
-    @CommandPermission("auraskills.storage")
+    @CommandPermission("auraskills.command.storage")
     public void onMergeSkills(CommandSender sender, String from, String to) {
         if (!(sender instanceof ConsoleCommandSender) && !(sender instanceof RemoteConsoleCommandSender)) {
             sender.sendMessage(ChatColor.RED + "Only console can execute this command!");
@@ -69,6 +70,38 @@ public class StorageCommand extends BaseCommand {
             throw new RuntimeException(e);
         }
         sender.sendMessage("Merged skill " + from + " into " + to + ", applying the higher skill level. Successfully applied to " + success + " users, failed to apply for " + failed + " users.");
+    }
+
+    @Subcommand("removeallmodifiers")
+    @CommandPermission("auraskills.command.storage")
+    public void removeAllModifiers(CommandSender sender) {
+        if (!(sender instanceof ConsoleCommandSender) && !(sender instanceof RemoteConsoleCommandSender)) {
+            sender.sendMessage(ChatColor.RED + "Only console can execute this command!");
+            return;
+        }
+        int failed = 0;
+        int success = 0;
+        int skipped = 0;
+        try {
+            StorageProvider storage = plugin.getStorageProvider();
+            for (UserState state : storage.loadStates(false)) {
+                if (state.statModifiers().isEmpty() && state.traitModifiers().isEmpty()){
+                    skipped++;
+                    continue;
+                }
+                UserState newState = new UserState(state.uuid(), state.skillLevels(), state.skillXp(), new HashMap<>(), new HashMap<>(), state.mana());
+                try {
+                    storage.applyState(newState);
+                    success++;
+                } catch (Exception e) {
+                    sender.sendMessage(ChatColor.RED + "Failed to apply user state of user " + state.uuid().toString());
+                    failed++;
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        sender.sendMessage("Removed all stat and trait modifiers for all users. Successfully applied to " + success + " users, failed to apply for " + failed + " users, " + skipped + " users had no modifiers to remove.");
     }
 
 }
