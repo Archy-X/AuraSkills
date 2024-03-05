e350849f4cdffadf6dee5df74115808c89b31025
Archy-X <archydevelopment@gmail.com>
2024-03-05 12:52:24 -0500

    Polyglot: Use default message file to update if no embedded

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     api("co.aikar:acf-core:0.5.1-SNAPSHOT")
     api("net.kyori:adventure-text-serializer-legacy:4.16.0")
     api("net.kyori:adventure-text-serializer-gson:4.16.0")
-    api("com.github.Archy-X:Polyglot:09de4adc9a")
+    api("com.github.Archy-X:Polyglot:4dad0ba55f")
     api("com.ezylang:EvalEx:3.0.4")
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("org.atteo:evo-inflector:1.3")
