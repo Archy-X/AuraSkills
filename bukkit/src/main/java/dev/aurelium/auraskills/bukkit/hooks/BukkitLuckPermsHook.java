825e4651c64750c3dcfd3469294297cc761d26a0
Erik Szabó <erik1szabo1@gmail.com>
2024-04-21 13:12:31 +0200

    Improve luckperms hook to optimize multiplier permission lookup

---
+++ bukkit/src/main/java/dev/aurelium/auraskills/bukkit/hooks/BukkitLuckPermsHook.java
@@ -0,0 +1,76 @@
+package dev.aurelium.auraskills.bukkit.hooks;
+
+import dev.aurelium.auraskills.common.AuraSkillsPlugin;
+import dev.aurelium.auraskills.common.hooks.Hook;
+import dev.aurelium.auraskills.common.hooks.LuckPermsHook;
+import net.luckperms.api.event.node.NodeAddEvent;
+import net.luckperms.api.event.node.NodeMutateEvent;
+import net.luckperms.api.event.node.NodeRemoveEvent;
+import net.luckperms.api.model.group.Group;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.types.InheritanceNode;
+import net.luckperms.api.node.types.PermissionNode;
+import net.luckperms.api.query.Flag;
+import net.luckperms.api.query.QueryOptions;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+import org.spongepowered.configurate.ConfigurationNode;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+public class BukkitLuckPermsHook extends LuckPermsHook {
+    private final Map<UUID, Set<PermissionAttachmentInfo>> permissionCache = new ConcurrentHashMap<>();
+
+    public BukkitLuckPermsHook(AuraSkillsPlugin plugin, ConfigurationNode config) {
+        super(plugin, config);
+
+        // This will run async saving main thread resources
+        luckPerms.getEventBus().subscribe(NodeMutateEvent.class, (event) -> {
+            if (event.isUser() && event.getTarget() instanceof User user) {
+                permissionCache.put(user.getUniqueId(), getMultiplierPermissions(user.getUniqueId()));
+            }
+
+            if (event.isGroup()) {
+                // Effective perms won't update instantly
+                plugin.getScheduler().scheduleAsync(() -> {
+                    permissionCache.keySet().forEach(key -> permissionCache.put(key, getMultiplierPermissions(key)));
+                }, 200, TimeUnit.MILLISECONDS);
+            }
+        });
+    }
+
+    public Set<PermissionAttachmentInfo> getMultiplierPermissions(Player player) {
+        return permissionCache.computeIfAbsent(
+                player.getUniqueId(),
+                (uuid -> getMultiplierPermissions(player.getUniqueId())));
+    }
+
+    private Set<PermissionAttachmentInfo> getMultiplierPermissions(UUID uuid) {
+        Player player = Bukkit.getPlayer(uuid);
+        if (player == null) {
+            return new HashSet<>(); // Return empty set if player not found
+        }
+
+        Set<PermissionAttachmentInfo> filteredPermissions = new HashSet<>(); // Initialize with expected capacity
+
+        for (PermissionAttachmentInfo pai : player.getEffectivePermissions()) {
+            String permission = pai.getPermission();
+            if (permission.startsWith("auraskills.multiplier.")) {
+                filteredPermissions.add(pai);
+            }
+        }
+
+        return filteredPermissions;
+    }
+
+    @Override
+    public Class<? extends Hook> getTypeClass() {
+        return BukkitLuckPermsHook.class;
+    }
+}
--- bukkit/src/main/java/dev/aurelium/auraskills/bukkit/user/BukkitUser.java
+++ bukkit/src/main/java/dev/aurelium/auraskills/bukkit/user/BukkitUser.java
@@ -3,10 +3,10 @@ package dev.aurelium.auraskills.bukkit.user;
 import dev.aurelium.auraskills.api.skill.Skill;
 import dev.aurelium.auraskills.api.user.SkillsUser;
 import dev.aurelium.auraskills.bukkit.AuraSkills;
+import dev.aurelium.auraskills.bukkit.hooks.BukkitLuckPermsHook;
 import dev.aurelium.auraskills.bukkit.skills.agility.AgilityAbilities;
 import dev.aurelium.auraskills.common.api.implementation.ApiSkillsUser;
 import dev.aurelium.auraskills.common.user.User;
-import dev.aurelium.auraskills.common.util.text.TextUtil;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -14,13 +14,10 @@ import org.bukkit.permissions.PermissionAttachmentInfo;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Locale;
+import java.util.Set;
 import java.util.UUID;
-import java.util.regex.Pattern;
 
 public class BukkitUser extends User {
-
-    // Permission pattern
-    private static final Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
 
     @Nullable
     private final Player player;
@@ -58,29 +55,77 @@ public class BukkitUser extends User {
             return 0.0;
         }
         double multiplier = 0.0;
-        for (PermissionAttachmentInfo info : player.getEffectivePermissions()) {
-            String permission = info.getPermission().toLowerCase(Locale.ROOT);
-            if (permission.startsWith("auraskills.multiplier.")) {
-                permission = TextUtil.replace(permission, "auraskills.multiplier.", "");
-                if (pattern.matcher(permission).matches()) { // Parse all skills multiplier
-                    multiplier += Double.parseDouble(permission) / 100.0;
-                } else if (skill != null) { // Skill specific multiplier
-                    String namespacedName = skill.toString().toLowerCase(Locale.ROOT);
-                    String plainName = skill.name().toLowerCase(Locale.ROOT);
-                    if (permission.startsWith(namespacedName)) {
-                        permission = TextUtil.replace(permission, namespacedName + ".", "");
-                    } else if (permission.startsWith(plainName)) {
-                        permission = TextUtil.replace(permission, plainName + ".", "");
-                    } else {
-                        continue;
-                    }
-                    if (pattern.matcher(permission).matches()) {
-                        multiplier += Double.parseDouble(permission) / 100.0;
-                    }
-                }
+
+        if (plugin.getHookManager().isRegistered(BukkitLuckPermsHook.class)) {
+            Set<PermissionAttachmentInfo> permissions = plugin.getHookManager().getHook(BukkitLuckPermsHook.class).getMultiplierPermissions(player);
+            for (PermissionAttachmentInfo permission : permissions) {
+                multiplier += getMultiplierFromPermission(permission.getPermission(), skill);
+            }
+            return multiplier;
+        } else {
+            long start = System.nanoTime();
+            for (PermissionAttachmentInfo permission : player.getEffectivePermissions()) {
+                multiplier += getMultiplierFromPermission(permission.getPermission(), skill);
+            }
+            long end = System.nanoTime();
+            System.out.println("With Player#getEffectivePermissions the multiplier lookup took: " + (end - start) + " nanoseconds.");
+        }
+
+        return multiplier;
+    }
+
+    private double getMultiplierFromPermission(String permission, @Nullable Skill skill) {
+        final String prefix = "auraskills.multiplier.";
+        if (!permission.startsWith(prefix)) {
+            return 0.0;
+        }
+
+        permission = permission.substring(prefix.length());
+
+        if (isNumeric(permission)) {
+            return Double.parseDouble(permission) / 100.0;
+        }
+
+        if (skill != null) {
+            String namespacedName = skill.toString().toLowerCase(Locale.ROOT) + ".";
+            String plainName = skill.name().toLowerCase(Locale.ROOT) + ".";
+
+            if (permission.startsWith(namespacedName)) {
+                permission = permission.substring(namespacedName.length());
+            } else if (permission.startsWith(plainName)) {
+                permission = permission.substring(plainName.length());
+            } else {
+                return 0.0;
+            }
+
+            if (isNumeric(permission)) {
+                return Double.parseDouble(permission) / 100.0;
             }
         }
-        return multiplier;
+
+        return 0.0;
+    }
+
+    private boolean isNumeric(String str) {
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+        boolean decimalSeen = false;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (i == 0 && c == '-') {
+                if (str.length() == 1) return false; // "-" alone is not a number
+                continue;
+            }
+            if (c == '.') {
+                if (decimalSeen || i == 0 || i == str.length() - 1)
+                    return false; // Double decimal or leading/trailing decimal
+                decimalSeen = true;
+            } else if (c < '0' || c > '9') {
+                return false; // Non-digit character
+            }
+        }
+        return true;
     }
 
     @Override
