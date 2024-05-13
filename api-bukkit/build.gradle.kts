32cd5055d253b518a3c41c012052e354e52748d8
Archy-X <archydevelopment@gmail.com>
2024-05-13 12:13:14 -0400

    Add format_title menu option and fix context issue

--- api-bukkit/build.gradle.kts
+++ api-bukkit/build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 
 dependencies {
     api(project(":api"))
-    api("dev.aurelium:slate:1.1.0") {
+    api("dev.aurelium:slate:1.1.1-SNAPSHOT") {
         exclude("org.yaml", "snakeyaml")
         exclude("org.spongepowered", "configurate-yaml")
     }
