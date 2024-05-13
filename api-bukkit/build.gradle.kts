8b2a0abaf7af5f809e1aae17db4113e873e722b5
Archy-X <archydevelopment@gmail.com>
2024-05-13 12:59:28 -0400

    Update Slate dependency to 1.1.2

--- api-bukkit/build.gradle.kts
+++ api-bukkit/build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 
 dependencies {
     api(project(":api"))
-    api("dev.aurelium:slate:1.1.1-SNAPSHOT") {
+    api("dev.aurelium:slate:1.1.2") {
         exclude("org.yaml", "snakeyaml")
         exclude("org.spongepowered", "configurate-yaml")
     }
