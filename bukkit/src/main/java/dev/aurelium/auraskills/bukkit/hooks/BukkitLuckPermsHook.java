e3be35a4881fd81ecc8e66d37baebc632a2a3588
Erik Szabó <erik1szabo1@gmail.com>
2024-08-05 09:52:58 +0200

    Change config key for permission cache

--- bukkit/src/main/java/dev/aurelium/auraskills/bukkit/hooks/BukkitLuckPermsHook.java
+++ bukkit/src/main/java/dev/aurelium/auraskills/bukkit/hooks/BukkitLuckPermsHook.java
@@ -37,7 +37,7 @@ public class BukkitLuckPermsHook extends LuckPermsHook implements Listener {
     public BukkitLuckPermsHook(AuraSkillsPlugin plugin, ConfigurationNode config) {
         super(plugin, config);
 
-        this.optimizePermissionLookup = config.node("optimize_permission_lookup").getBoolean(true);
+        this.optimizePermissionLookup = config.node("use_permission_cache").getBoolean(true);
 
         if (!this.optimizePermissionLookup) return;
 
