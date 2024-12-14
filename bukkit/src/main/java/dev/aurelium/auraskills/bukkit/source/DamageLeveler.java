d66e3c84e8c2a16ef02cb09712c133bf2c3af255
Intybyte <vaan.testwork@gmail.com>
2024-12-14 12:41:10 +0100

    Unused import

--- bukkit/src/main/java/dev/aurelium/auraskills/bukkit/source/DamageLeveler.java
+++ bukkit/src/main/java/dev/aurelium/auraskills/bukkit/source/DamageLeveler.java
@@ -9,7 +9,6 @@ import dev.aurelium.auraskills.bukkit.AuraSkills;
 import dev.aurelium.auraskills.common.source.SourceTypes;
 import dev.aurelium.auraskills.common.user.User;
 import org.bukkit.Location;
-import org.bukkit.damage.DamageSource;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
