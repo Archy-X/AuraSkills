ad73cb2f34437851f0d4c02d66b2e5e972a0f37f
Fly1337 <64212091+Fly1337@users.noreply.github.com>
2025-02-20 20:11:36 +0100

    Add configurable requirements for blocks (#342)

---
+++ bukkit/src/main/java/dev/aurelium/auraskills/bukkit/requirement/blocks/ExcludedWorldNode.java
@@ -0,0 +1,24 @@
+package dev.aurelium.auraskills.bukkit.requirement.blocks;
+
+import dev.aurelium.auraskills.bukkit.AuraSkills;
+import org.bukkit.entity.Player;
+
+public class ExcludedWorldNode extends RequirementNode {
+
+    private final String[] worlds;
+
+    public ExcludedWorldNode(AuraSkills plugin, String[] worlds, String message) {
+        super(plugin, message);
+        this.worlds = worlds;
+    }
+
+    @Override
+    public boolean check(Player player) {
+        for (String world : worlds) {
+            if (player.getWorld().getName().equalsIgnoreCase(world)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
