a0efae8e0226bbf899de96670966a4db09f492f6
Archy-X <63976867+Archy-X@users.noreply.github.com>
2025-03-16 17:28:48 -0400

    Add percent_format and current_xp_format options to skills menu

--- bukkit/src/main/java/dev/aurelium/auraskills/bukkit/menus/MenuFileManager.java
+++ bukkit/src/main/java/dev/aurelium/auraskills/bukkit/menus/MenuFileManager.java
@@ -2,6 +2,7 @@ package dev.aurelium.auraskills.bukkit.menus;
 
 import dev.aurelium.auraskills.api.registry.NamespacedRegistry;
 import dev.aurelium.auraskills.bukkit.AuraSkills;
+import dev.aurelium.auraskills.bukkit.menus.shared.SkillItem;
 import dev.aurelium.auraskills.common.api.ApiAuraSkills;
 import dev.aurelium.auraskills.common.util.file.FileUtil;
 import org.spongepowered.configurate.ConfigurationNode;
@@ -42,6 +43,10 @@ public class MenuFileManager {
         }
 
         int menusLoaded = plugin.getSlate().loadMenus();
+
+        // Post load operations
+        SkillItem.loadFormats(plugin);
+
         plugin.getLogger().info("Loaded " + menusLoaded + " menus");
     }
 
