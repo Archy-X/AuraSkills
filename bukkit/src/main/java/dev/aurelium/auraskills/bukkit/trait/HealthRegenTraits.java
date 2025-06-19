04d8d105f8b730f440ca3a1bb775b4812a626850
R00tB33rMan <rootb33rman@gmail.com>
2025-06-19 01:32:25 -0400

    Formatting oopsie

--- bukkit/src/main/java/dev/aurelium/auraskills/bukkit/trait/HealthRegenTraits.java
+++ bukkit/src/main/java/dev/aurelium/auraskills/bukkit/trait/HealthRegenTraits.java
@@ -117,7 +117,7 @@ public class HealthRegenTraits extends TraitImpl {
             if (!(player.getHealth() < attribute.getValue())) return;
 
             if (regenCondition.apply(player)) {
-            double amountGained = Math.min(trait.optionDouble("base") + user.getBonusTraitLevel(trait), attribute.getValue() - player.getHealth());
+                double amountGained = Math.min(trait.optionDouble("base") + user.getBonusTraitLevel(trait), attribute.getValue() - player.getHealth());
 
                 final double gainThreshold = 0.001;
                 if (amountGained < gainThreshold) {
