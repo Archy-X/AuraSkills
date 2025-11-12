1c65878320fd4c07d295c4ae65a0fad3ddd08df2
Archy-X <63976867+Archy-X@users.noreply.github.com>
2025-11-11 20:02:27 -0500

    Add menu item and template variants
    - Allow duplicate template items using pos list

--- api-bukkit/build.gradle.kts
+++ api-bukkit/build.gradle.kts
@@ -13,10 +13,10 @@ repositories {
 
 dependencies {
     api(project(":api"))
-    api("dev.aurelium:slate:1.1.17") {
+    api("dev.aurelium:slate:1.2.0") {
         exclude("org.spongepowered", "configurate-yaml")
     }
-    // api(files("../../Slate/build/libs/Slate-1.1.17-all.jar"))
+    api(files("../../Slate/build/libs/Slate-1.2.0-all.jar"))
     compileOnly("org.jetbrains:annotations:24.1.0")
     compileOnly("org.spigotmc:spigot-api:1.21.5-R0.1-SNAPSHOT")
 }
